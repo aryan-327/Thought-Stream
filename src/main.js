2c9432b4c6a788f0d9c1fe2d29d02b849db40cf9
aryan-327 <aruna.aryanpatnaik@gmail.com>
2026-09-25 12:13:34 +0530

    use env variables for firebase config

--- src/main.js
+++ src/main.js
@@ -13,13 +13,13 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBoIp1u3VMLZGDtCJ4zQ1WjJCnZ7g4yXD0",
-    authDomain: "thought-stream-36106.firebaseapp.com",
-    projectId: "thought-stream-36106",
-    storageBucket: "thought-stream-36106.firebasestorage.app",
-    messagingSenderId: "872785463424",
-    appId: "1:872785463424:web:fc068bc909767704c17e65",
-    measurementId: "G-2MZHM6VSRE"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
